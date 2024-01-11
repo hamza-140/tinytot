023b39ccc07b031d6ce7ebfcb22cb8261ba351a3
Hamza Syed <sahamzashah19@gmail.com>
2024-01-11 21:43:50 +0500

    updated

---
+++ main.py
@@ -0,0 +1,26 @@
+def foo():
+    print(1)
+def soo():
+    print(0)
+def zoo():
+    foo()
+    soo()
+
+def koo():
+    foo()
+    soo()
+    soo()
+def loo():
+    foo()
+    foo()
+    soo()
+
+zoo()
+loo()
+soo()
+zoo()
+soo()
+loo()
+loo()
+zoo()
+foo()
