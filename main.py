d27803a5d386ba65887107ed703daabf46704252
Hamza Syed <sahamzashah19@gmail.com>
2024-01-26 17:47:28 +0500

    updated

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-# # Hardcoded example values
-# T_input = "2, 1, 3, 4"
-# S_input = "3, 2, 5, 6"
+# pip install pywhatkit
+import pywhatkit as kit
 
+# Specify the phone number (with country code) and the message
+phone_number = "+1234567890"
+message = "Hello from Python! This is an instant WhatsApp message."
 
-# # Splitting the input strings into lists of integers
-# T = [int(val) for val in T_input.split(',')]
-# S = [int(val) for val in S_input.split(',')]
-
-# # Sorting seats in descending order
-# S.sort(reverse=True)
-
-# countPassengers = sum(T)
-# print("Passengers {}\nSeats {}".format(countPassengers,S))
-# countBuses = 0
-# inBus = 0
-
-# for seats in S:
-#     if inBus < countPassengers:
-#         countBuses += 1
-
-#     inBus += seats
-#     print("Seats Occupied {} \nBuses Count {}".format(inBus,seats))
-
-
-# print(countBuses)
-
+# Send the message instantly
+kit.sendwhatmsg_instantly(phone_number, message)
