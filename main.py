c45c125747b9dc074461375b9782dcb47c817217
Hamza Syed <sahamzashah19@gmail.com>
2024-01-27 03:39:59 +0500

    updated

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# pip install pywhatkit
-import pywhatkit as kit
 
-# Specify the phone number (with country code) and the message
-phone_number = "+1234567890"
-message = "Hello from Python! This is an instant WhatsApp message."
+import json
 
-# Send the message instantly
-kit.sendwhatmsg_instantly(phone_number, message)
+person_dict = {'name': 'Bob',
+'age': 12,
+'children': None
+}
+person_json = json.dumps(person_dict)
