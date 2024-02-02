49e18b62d168498ed7202e4fd3631b55f62de3b3
Hamza Syed <sahamzashah19@gmail.com>
2024-02-02 20:52:12 +0500

    updated

--- admin/src/pages/Lesson.js
+++ admin/src/pages/Lesson.js
@@ -196,7 +196,7 @@ const Lesson = () => {
   return (
     <div>
       <p>{isSpeaking ? 'Speaking...' : 'Tap to speak'}</p>
-      <button onClick={() => speakText('Hello, world!')} disabled={isSpeaking}>
+      <button onClick={() => speakText('Hello!')} disabled={isSpeaking}>
         Speak Text
       </button>
       <button onClick={stopSpeaking} disabled={!isSpeaking}>
