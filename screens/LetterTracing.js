d0a59e87b442ef073fdcb7f47619410c1f647fa0
Hamza Syed <sahamzashah19@gmail.com>
2024-02-05 21:40:54 +0500

    Add react-native-tts and use it to speak a text.

--- screens/LetterTracing.js
+++ screens/LetterTracing.js
@@ -2,13 +2,25 @@ import React, {useContext} from 'react';
 import {Button, Text} from 'react-native';
 import {View} from 'react-native';
 import {Context} from '../context/AuthContext';
+import Tts from 'react-native-tts';
+
 const LetterTracing = () => {
   const {signout} = useContext(Context);
+  const tts = () => {
+    Tts.speak('Hello!', {
+      androidParams: {
+        KEY_PARAM_PAN: -1,
+        KEY_PARAM_VOLUME: 0.5,
+        KEY_PARAM_STREAM: 'STREAM_MUSIC',
+      },
+    });
+  };
   return (
     <View>
+      <Text style={{color: 'black'}}>Hekko</Text>
       <Text>Account Screen</Text>
 
-      <Button title="Sign Out" onPress={signout}></Button>
+      <Button title="Sign Out" onPress={tts}></Button>
     </View>
   );
 };
