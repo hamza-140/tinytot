4fd3b99b2800c038de30535fbe79ca7cc7454dcd
Hamza Syed <sahamzashah19@gmail.com>
2024-02-07 16:39:37 +0500

    Change the text spoken by the TTS engine from "Hello Me!" to "Hello!"

--- screens/LetterTracing.js
+++ screens/LetterTracing.js
@@ -7,7 +7,7 @@ import Tts from 'react-native-tts';
 const LetterTracing = () => {
   const {signout} = useContext(Context);
   const tts = () => {
-    Tts.speak('Hello Me!', {
+    Tts.speak('Hello!', {
       androidParams: {
         KEY_PARAM_PAN: -1,
         KEY_PARAM_VOLUME: 0.5,
