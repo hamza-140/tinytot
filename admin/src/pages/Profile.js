738171adadc2bc184a849f8eb410e896f578186f
Hamza Syed <sahamzashah19@gmail.com>
2024-02-20 00:49:33 +0500

    Update user profile label

--- admin/src/pages/Profile.js
+++ admin/src/pages/Profile.js
@@ -6,7 +6,7 @@ const Profile = () => {
   return (
     <div className="dashboard-container">
       <SideBar menu={sidebar_menu} />
-      <div className="dashboard-body">Profile</div>
+      <div className="dashboard-body">User Profile</div>
     </div>
   );
 };
